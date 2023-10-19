e922d4a9b492be2cb3fe21236a18ba57762cbe69
Kevin Mullins <kevin_mullins@appracatappra.com>
2023-10-19 11:09:16 -0500

    Initial Commit

---
+++ Tests/AppStoreManagerTests/AppStoreManagerTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import AppStoreManager
+
+final class AppStoreManagerTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
