834a62cac0fece362336c5f2141f02d4807a3593
Kevin Mullins <kevin_mullins@appracatappra.com>
2026-06-12 10:30:23 -0500

    Update to OS 26
    Update to OS 26

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 6.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "AppStoreManager",
-    platforms: [.iOS(.v18), .macOS(.v15), .tvOS(.v18), .watchOS(.v11)],
+    platforms: [.iOS(.v26), .macOS(.v26), .tvOS(.v26)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,9 +15,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Appracatappra/LogManager", .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/Appracatappra/SimpleSerializer", .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/Appracatappra/SwiftletUtilities", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/Appracatappra/LogManager", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/Appracatappra/SimpleSerializer", .upToNextMajor(from: "2.5.0")),
+        .package(url: "https://github.com/Appracatappra/SwiftletUtilities", .upToNextMajor(from: "2.1.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
